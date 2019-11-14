df4e6efd3b3b05508715e2b86c684f9b0035d3f7
Gar <gar@lonelydatu.com>
2019-11-14 11:07:57 -0500

    one

--- dev/std_300x250/_js/main.js
+++ dev/std_300x250/_js/main.js
@@ -5,15 +5,17 @@ function start() {
 	const tl = new TimelineMax()
 	tl.set(".frame1", {opacity:1})
 
+
+	const tlPlaerys = new TimelineMax()
 	const TIME_SHOOT = 5
 
-	tl.add('player', .3)
-	tl.from(".draw_shoot_player", TIME_SHOOT, {x:"+=10", y:"+=230", ease:Power2.easeOut}, 'player')
-	tl.from(".draw_shoot_lines", TIME_SHOOT*.9, {x:"+=10", y:"+=230", ease:Power2.easeOut}, 'player')
+	tlPlaerys.add('player', .3)
+	tlPlaerys.from(".draw_shoot_player", TIME_SHOOT, {x:"+=10", y:"+=230", ease:Power2.easeOut}, 'player')
+	tlPlaerys.from(".draw_shoot_lines", TIME_SHOOT*.9, {x:"+=10", y:"+=230", ease:Power2.easeOut}, 'player')
 
-	tl.add('defend', .5)
-	tl.from(".draw_defend_player", TIME_SHOOT, {x:"-=20", y:"+=230", ease:Power2.easeOut}, 'defend')
-	tl.from(".draw_defend_lines", TIME_SHOOT*.9, {x:"-=20", y:"+=230", ease:Power2.easeOut}, 'defend')
+	tlPlaerys.add('defend', .5)
+	tlPlaerys.from(".draw_defend_player", TIME_SHOOT, {x:"-=20", y:"+=230", ease:Power2.easeOut}, 'defend')
+	tlPlaerys.from(".draw_defend_lines", TIME_SHOOT*.9, {x:"-=20", y:"+=230", ease:Power2.easeOut}, 'defend')
 
 
 
@@ -24,22 +26,22 @@ function start() {
 	tlText.from(".t1c", .01, {opacity:0}, "+=.3")
 	tlText.from(".t1d", .01, {opacity:0}, "+=.3")
 
-	// tl.add(tlText, .8)
+	tl.add(tlText, .8)
 
 
-	tl.add("f2", 4)
+	// tl.add("f2", 1)
 
 	// tl.set(".frame2", {opacity:1}, "f2")
 	
-	tl.to(".frame1", .2, {opacity:0}, "f2")
-	tl.to(".frame2", .4, {opacity:1})
+	tl.to(".frame1", .2, {opacity:0}, 3.5)
+	tl.to(".frame2", .2, {opacity:1})
 
 
 	tl.from(".phone", .4, {y:"+=150"})
 	tl.from(".illust", .4, {scale:0})
 
 	tl.from(".t2", .3, {opacity:0}, "+=.1")
-	tl.from(".cta", .3, {opacity:0}, "+=.5")
+	tl.from(".cta", .3, {opacity:0}, "+=.9")
 	tl.to(".cta", .2, {opacity:0}, "+=2")
 	tl.from([".cta2", ".legal"], .4, {opacity:0})
 	
